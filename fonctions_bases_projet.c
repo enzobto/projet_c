b0f624d8004bda2ac73162e5caefdcdcf957d00f
EliOvni <57061197+eliotclerc@users.noreply.github.com>
2021-12-22 15:37:39 +0100

    Fonctions de bases
    En gros j'ai 3 fonctions de base :
    - initialiser une map
    - afficher une map
    - changer un élément de la map
    et j'ai fait gaff à print des erreurs et tout si on utilisais mal les fonctions comme ça si y'a des erreurs ce sera moins chiant à trouver d'où ça vient ;)

---
+++ fonctions_bases_projet.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#define LONGUEUR 14
+#define LARGEUR 14
+
+
+void initialise_map(int largeur, int longueur, char mat[largeur][longueur]) {
+  for (int i = 0; i < largeur; i++){
+    for (int j = 0; j < longueur; j++){ //rempli une map de #
+      mat[i][j] ='#';
+    }
+  }
+}
+
+void affiche_map(int largeur, int longueur,char map[largeur][longueur]){
+for(int i = 0; i < largeur; i++){
+ for(int j = 0; j < longueur; j++){ //affiche la map
+   printf("%c",map[i][j]);
+ }
+   printf("\n");
+ }
+
+}
+
+void change_case(int largeur, int longueur, int abs, int ord, char element,char map[largeur][longueur]){
+  if(longueur > abs && largeur > ord){    
+    map[ord][abs] = element; //place un element sur la map
+    }
+  else{
+    printf("On tente de placer l'élément en dehors du tableau");
+  }
+}
+
+
+int main(){
+char map[LARGEUR][LONGUEUR] = {{}};
+initialise_map(LARGEUR, LONGUEUR, map);
+change_case(LARGEUR, LONGUEUR, 7, 7, '@', map);
+affiche_map(LARGEUR, LONGUEUR, map);
+}
