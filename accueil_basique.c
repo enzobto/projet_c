36e03e00feb0ed5a5863012bb5c5c2c9f9216fc1
EmileUwU <96500990+EmileUwU@users.noreply.github.com>
2021-12-23 18:30:25 +0100

    Update accueil_basique.c

--- accueil_basique.c
+++ accueil_basique.c
@@ -1,28 +1,79 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int joueur(){
+int joueur(int n, char pseudo1[n],char pseudo2[n], int j){
 
-    char pseudo[100];
     printf("                ------------------------------------------------------\n");
     printf("                |                                                    |\n");
     printf("                |                Bienvenue  dans                     |\n");
     printf("                |                                                    |\n");
     printf("                |                   POLYTOPIA                        |\n");
     printf("                |                                                    |\n");
-    printf("                | Peux-tu entrer ton pseudo ?");
-    scanf("%s", pseudo);
+    printf("                | Il y a 1 ou 2 joueurs ?                            |\n");
+    printf("                ------------------------------------------------------\n");
+    scanf("%d\n", &j);
+    
+    while((j != 1) && (j != 2)){
 
-    printf("                | Salut %s, tu es prêt a jouer ?                   |\n",pseudo);
+        if((j != 1) && (j != 2)){
+        printf("Entrez 1 ou 2 !\n");
+    }
+        scanf("%d\n", &j);
+        
+    }
+
+    if (j == 1){
+        printf("Ton pseudo est : "); //probleme : la fonction demande pseudo1 AVANT d'executer cette ligne
+        scanf("%s", pseudo1);
+        printf("\n");
+        pseudo2 = "IA";
+    } else if (j == 2){
+        printf("Le pseudo du joueur 1 est : ");
+        scanf("%s", pseudo1);
+        printf("\n");
+        printf("Le pseudo du joueur 2 est : ");
+        scanf("%s", pseudo2);
+        printf("\n");
+    }
+
+    printf("                ------------------------------------------------------\n");
+    printf("                | Salut %s, tu es prêt a jouer contre %s ?           |\n", pseudo1, pseudo2);
     //printf("                        |\n");
     printf("                |                                                    |\n");
-    printf("                | Oui                                                |\n");
-    printf("                | Non                                                |\n");
+    printf("                | Oui : entrez 'y'                                   |\n");
+    printf("                | Non : entrez 'n'                                   |\n");
     printf("                ------------------------------------------------------\n");
-    return 0;
+
+    char c;
+    c = 'a';
+
+    while((c != 'y') && (c != 'n')){
+    c = getchar(); // On attends que le joueur dise oui ou non
+    if((c != 'y') && (c != 'n')){
+        printf("Entrez 'y' ou 'n' !\n");
+    }
+    }
+
+    if (c == 'y'){
+        return 42;
+    } else if (c == 'n'){
+        return 12;
+}
 }
 
 int main(void){
-  joueur();
+    char pseudo1[100] = {'\0'}; //Nom du premier joueur
+    char pseudo2[100] = {'\0'}; // Nom du second joueur ou IA
+    int j = 0; // le nb de joueurs
+    if (joueur(100, pseudo1, pseudo2, j) == 42){
+        printf("Normalement ca marche !\n");
+        return EXIT_SUCCESS;
+        // lancer le jeu
+    } else {
+        printf("Au plaisir de vous revoir !\n");
+        return EXIT_SUCCESS;
+        // ferme le jeu
+    }
+
 }
- 
